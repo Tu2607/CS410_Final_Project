1c941f2ce3b1ec91e59ff02ba91b3eefa7fbcce8
Tu Vu <tuvu2607@gmail.com>
2020-05-22 13:18:54 -0700

    Update source.py

--- source.py
+++ source.py
@@ -1,1 +1,18 @@
 #This is the source code for the project
+
+#The function to read in key entered from keyboard
+def parsing_args():
+    return 0
+
+def main():
+
+    keytosound = {} #dictionary for 
+    while True:
+        #This is the loop that will keep recording the sound
+        a = 0 #Temp code
+
+    return 0
+
+if __name__ == '__main__':
+    main()
+    
