96755695d69950e15999b15fb149838a46fe1540
Tu Vu <tuvu2607@gmail.com>
2020-05-26 23:08:34 -0700

    Some ideas

--- draft.py
+++ draft.py
@@ -1,52 +1,19 @@
 import numpy as np
 import sys
-from tkinter import *
-import pygame
-from pygame.locals import *
+import frequency_table
 
-# initialize the pygame modules
-pygame.init()
+##THIS IS ALL A TEST
+#create an list of sine waves for each note
+def list_of_wave():
+  amplitude = np.iinfo(np.int16).max
+  t = np.linspace(0., 3., 48000) 
+  notes = []
+  for value in frequency_table.frequency.values():
+    x =(0.5 * amplitude * np.sin(2 * np.pi * value * t)).astype(np.int16)
+    notes.append(x)
 
-#If we're using a dictionary of keys...
-#Have a file that basically have all the keys name so we can just use
-#np.loadtxt to read it in
-names = np.loadtxt("filename")
-keys = {}
-for i in range (names.shape[0]):
-  keys[names[i]] = frequency # <-- the frequency we can just calculate it by hand and add it into the file
+  return notes  
 
 
-"""
-def C_sharp(): 
-  note.set("C#")
-  sound = pygame.mixer.Sound(_____)
-  sound.play()
-  return
 
-def D_sharp(): 
-  note.set("D#") 
-  sound = pygame.mixer.Sound(_____) 
-  sound.play()
-  return
-
-def F_sharp(): 
-  note.set("F#")
-  sound = pygame.mixer.Sound(_____)
-  sound.play()
-  return 
-  
-def G_sharp(): 
-  note.set("G#")
-  sound = pygame.mixer.Sound(_____) 
-  sound.play()
-  return
-
-def B_flat(): 
-  note.set("Bb")
-  sound = pygame.mixer.Sound(_____)
-  sound.play()
-  return
-
-# and so on...
-"""
 
