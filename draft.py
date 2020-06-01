33f0340f6f0dcc322c690afa1f5f9f00a6df3978
Tu Vu <tuvu2607@gmail.com>
2020-06-01 11:06:26 -0700

    Modified draft.py to have new mapping

--- draft.py
+++ draft.py
@@ -8,36 +8,27 @@ import samplerate
 #########Test code ##############
 def read_notes():
   data, sps = sf.read("C3.wav")
-  return data
+  return data, sps
 
 #Might be a little bit of hardcoding with the MIDI key value for C3
-def pitchshift(data, n):
-  shift = 1/ (2** (n/12)) #Shift by one semitone
-  converter = 'sinc_fastest'
-  output = samplerate.resample(data, shift, converter)
-  
-  sf.write("test" + str(n) + ".wav", output, 44100, subtype= 'PCM_24')
-
-def list_of_wave():
-  amplitude = np.iinfo(np.int16).max
-  t = np.linspace(0., 1., 48000)  #Check my math and code here
+def pitchshift(data, amount_of_notes):
   notes = []
-  for value in frequency_table.frequency.values():
-    x = (0.5 * amplitude * np.sin(2 * np.pi * value * t)).astype(np.int16)
-    notes.append(x)
-  return np.array(notes)
-
+  for n in range(amount_of_notes):
+    shift = 1/ (2** (n/12)) #Shift by one semitone
+    converter = 'sinc_fastest'
+    output = samplerate.resample(data, shift, converter)
+    notes.append(output)
+  return notes
+  
 def main(): 
   keys = np.loadtxt("keys-copy.txt", dtype= str)
-  array = read_notes()
-  pitchshift(array,1)
-  pitchshift(array,2)
-  sines = list_of_wave() #Return the a 2d array with each row is a sine wave for each notes
+  array, rate = read_notes()
+  list_of_notes = pitchshift(array,36) 
 
-  pygame.mixer.init(44100, -16, 1, 2048) #Initialize the mixer. This must happen before anything else with pygame sound
+  pygame.mixer.init(rate, -16, 2) #Initialize the mixer. This must happen before anything else with pygame sound
 
   #Mapping, uncomment to have the keyboard map
-  sounds = map(pygame.sndarray.make_sound, sines) #Copying the line 100 from the git.  The idea is the same
+  sounds = map(pygame.sndarray.make_sound, list_of_notes) #Copying the line 100 from the git.  The idea is the same
   keyboard = dict(zip(keys, sounds)) #Ziping the key to sound
 
 if __name__ == "__main__":
