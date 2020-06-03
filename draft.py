41e7f9ea21317ddc3ed07ba91e32adcc9dbacc53
Tu Vu <tuvu2607@gmail.com>
2020-06-03 16:02:33 -0700

    Bart code and a new draft

--- draft.py
+++ draft.py
@@ -1,40 +1,56 @@
-import numpy as np
-import sys
-import pygame
-import soundfile as sf
-import samplerate
+#Inspiration from Bart Massey Simple Sampler program
+#All credits go to him
 
-#########Test code ##############
-def read_notes():
-  data, sps = sf.read("C3.wav")
-  return data, sps
+import sampler
+import numpy as np 
+from scipy.io.wavfile import read, write
+import scipy.signal
+import scipy.fftpack
+import matplotlib.pyplot as plt
 
-#Might be a little bit of hardcoding with the MIDI key value for C3
-def pitchshift(data, amount_of_notes):
-  notes = []
-  for n in range(amount_of_notes):
-    #shift = 1/ (2** (n/12)) #Shift by one semitone
-    shift = 2**(n/12)
-    converter = 'sinc_fastest'
-    output = samplerate.resample(data, shift, converter)
-    notes.append(output)
-  return notes
-  
-def main(): 
-  keys = np.loadtxt("keys-copy.txt", dtype= str)
-  array, rate = read_notes()
-  list_of_notes = pitchshift(array,36) 
+def wavwrite(f, s):
+    write(f, 44100, (32767 * s).astype(np.int16))
 
-  pygame.mixer.init(rate, -16, 2) #Initialize the mixer. This must happen before anything else with pygame sound
+A3 = np.sin(np.linspace(0., 2. * np.pi * 220, 44100))
+wavwrite("A3.wav", A3)
 
-  #Mapping, uncomment to have the keyboard map
-  sounds = map(pygame.sndarray.make_sound, list_of_notes) #Copying the line 100 from the git.  The idea is the same
-  keyboard = dict(zip(keys, sounds)) #Ziping the key to sound
+notes = []
+notes.append(A3)
 
-if __name__ == "__main__":
-  main()
+def smoothing(array, window_len = 11, window= 'blackman'):
+    if array.ndim != 1:
+        raise ValueError("Not 1d array")
+    
+    if array.size < window_len:
+        raise ValueError("Array too small")
+
+    s = np.r_[array[window_len-1:0:-1], array, array[-2:-window_len-1:-1]]
+
+    w = eval('np.'+window+'(window_len)')
+
+    y = np.convolve(w/w.sum(), s, mode = 'valid')
+    return y
+
+def plotting(array):
+    x = np.arange(44100)
+    plt.plot(x,array)
+    plt.show()
+
+def list_of_notes(notes_count, base_note):
+    loop = sampler.Loop(base_note)
+    for n in range(notes_count):
+        if n != 0:
+            factor = 2**(n / 12) #semitones factor
+            new_note = loop.sample(220 * factor, 44100)
+            plotting(new_note)
+            new_note = smoothing(new_note) 
+            wavwrite(str(n) + ".wav", new_note)
+            notes.append(new_note)
+
+list_of_notes(3, A3)
 
 
+        
 
 
 
